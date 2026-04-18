890a700b795a4db905d5a1fb319909440b56bcb8
4chuck <lakshmikharvi91@gmail.com>
2026-04-18 14:03:46 +0530

    deploy fix

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
 const API_URL =
   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://127.0.0.1:8000"
-    : "https://your-buddy-2.onrender.com";
+    : "https://your-buddy-utp8.onrender.com";
 
 // Elements
 const dropArea = document.getElementById("dropArea");
